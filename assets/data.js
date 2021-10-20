f2d7471173394e209f3c2e238ff3fc4930c0bc0e
elvinciqueira <elvinciqueira@gmail.com>
2021-10-20 14:16:41 -0300

    server side rendering

--- assets/data.js
+++ assets/data.js
@@ -1,15 +1,21 @@
 import data from './restaurants.js';
 
-const newData = data.reduce((acc, { menus, ...restaurant }) => ({
-  restaurants: [
-    ...acc.restaurants,
-    [restaurant.id, { ...restaurant, menus: menus.map(menu => menu.id) }]
-  ],
-  menus: [
-    ...acc.menus,
-    ...menus.map((menu) => [menu.id, { ...menu, restaurantId: restaurant.id }]),
-  ]
-}), { restaurants: [], menus: [] });
+const newData = data.reduce(
+  (acc, { menus, ...restaurant }) => ({
+    restaurants: [
+      ...acc.restaurants,
+      [restaurant.id, { ...restaurant, menus: menus.map((menu) => menu.id) }],
+    ],
+    menus: [
+      ...acc.menus,
+      ...menus.map((menu) => [
+        menu.id,
+        { ...menu, restaurantId: restaurant.id },
+      ]),
+    ],
+  }),
+  { restaurants: [], menus: [] }
+);
 
 const restaurants = new Map(newData.restaurants);
 const menus = new Map(newData.menus);
@@ -17,4 +23,4 @@ const menus = new Map(newData.menus);
 export default {
   restaurants,
   menus,
-}
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+import http from 'http';
+import fs from 'fs';
+import ejs from 'ejs';
+import data from './assets/data.js';
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  if (req.url.match(/\.js$/)) {
+    const fileStream = fs.createReadStream(`./assets${req.url}`);
+    res.writeHead(200, {
+      'Content-type': 'text/javascript',
+    });
+    fileStream.pipe(res);
+  } else {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const templateData = {
+      menus: Array.from(data.menus.values())
+        .slice(0, 3)
+        .map((menu) => ({
+          ...menu,
+          restaurant: {
+            name: data.restaurants.get(menu.restaurantId).name,
+          },
+        })),
+    };
+    ejs.renderFile('./templates/index.ejs', templateData, (err, str) => {
+      if (err) console.log(err);
+      res.write(str);
+      res.end();
+    });
+  }
+  res.end();
+});
+
+server.listen(3001, (err) => {
+  if (err) console.error(err);
+  console.log('listen on port 3001');
+});
