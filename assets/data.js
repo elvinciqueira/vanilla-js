37e8d66754f06189d9b29ef83a7a185cd2ffaa16
elvinciqueira <elvinciqueira@gmail.com>
2021-10-20 13:56:52 -0300

    initial commit

---
+++ assets/data.js
@@ -0,0 +1,12 @@
+import data from './restaurants.js';
+
+const newData = data.reduce((acc, { menus, ...restaurant }) => ({
+  restaurants: [
+    ...acc.restaurants,
+    [restaurant.id, { ...restaurant, menus: menus.map(menu => menu.id) }]
+  ],
+  menus: [
+    ...acc.menus,
+    ...menus.map((menu) => [menu.id, { ...menu, restaurantId: restaurant.id }]),
+  ]
+}), { restaurants: [], menus: [] });
